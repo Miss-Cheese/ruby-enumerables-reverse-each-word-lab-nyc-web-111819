9d0c38d25c1602c851e40d0e3d513a1aac02845c
Natalie Galligan <nat.galligan@gmail.com>
2019-11-13 20:33:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   puts "1-----------------"
   sentence_array = "Hello there, and how are you?".split(/ /)
   sentence_array.each do |binkbutt|
-    puts binkbutt
+    puts binkbutt.reverse
   end
   puts "2-----------------"
 end
