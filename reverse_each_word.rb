afc7d729ad7e7993047ad267b68f9fcd9eb0f150
Natalie Galligan <nat.galligan@gmail.com>
2019-11-13 20:13:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
-def reverse_each_word(array)
-  yield
-end
-
-reverse_each_word("Hello there, and how are you?") { |index| "#{index.reverse_each_word}"}
+# def reverse_each_word(array)
+#   yield
+# end
+#
+# reverse_each_word("Hello there, and how are you?") { |index| "#{index.reverse_each_word}"}
+# #
