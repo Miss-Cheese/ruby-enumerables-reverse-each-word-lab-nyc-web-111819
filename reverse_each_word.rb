7127c4a67e887057b7d175807a7cfcc2cd17a589
Natalie Galligan <nat.galligan@gmail.com>
2019-11-13 20:52:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence)
-  sentence_array = sentence.split(/ /)
-  result = []
-  sentence_array.each do |binkbutt|
-    result << binkbutt.reverse
-  end
-  result.join(" ")
-end
+# def reverse_each_word(sentence)
+#   sentence_array = sentence.split(/ /)
+#   result = []
+#   sentence_array.each do |binkbutt|
+#     result << binkbutt.reverse
+#   end
+#   result.join(" ")
+# end
 
 
 def reverse_each_word(sentence)
